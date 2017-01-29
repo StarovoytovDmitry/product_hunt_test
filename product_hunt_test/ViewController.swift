8c55034fd4abc594e2aa738f0537d6ace9673480
Дмитрий <delfin@MacBook-Air-Dmitrij.local>
2017-01-29 17:12:42 +0300

    init project

---
+++ product_hunt_test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  product_hunt_test
+//
+//  Created by Дмитрий on 29.01.17.
+//  Copyright © 2017 js. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
